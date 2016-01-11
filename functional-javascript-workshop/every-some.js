560658537154d7f3e1aa82c6ec67cbb0afb7e52d
Eric Kizaki <erickizaki@gmail.com>
2016-01-11 15:35:43 -0600

    Every and Some

---
+++ functional-javascript-workshop/every-some.js
@@ -0,0 +1,13 @@
+function checkUsersValid(goodUsers) {
+  return function allUsersValid(submittedUsers) {
+    // SOLUTION GOES HERE
+    return submittedUsers.every(function(submittedValue){
+    	return goodUsers.some(function(goodValue){
+    		return submittedValue.id === goodValue.id;
+    	})
+    });
+
+  };
+}
+
+module.exports = checkUsersValid;
